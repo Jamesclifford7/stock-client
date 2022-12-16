8d3ecfd42083c15c42901a8f5c801a88750f5a79
James Clifford <jamesclifford7@gmail.com>
2022-12-16 16:09:21 -0700

    refactored to use react-chartjs-2

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,22 +3,45 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import styled from 'styled-components'
 import axios from "axios"
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
 
 export default function Home() {
     const [stock, setStock] = useState<string>('')
     const [eps, setEps] = useState<string>('')
     const [quarterlyEarningsGrowthYOY, setQuarterlyEarningsGrowthYOY] = useState<string>('')
-    const [priceData, setPriceData] = useState<Object[]>([])
+    // const [priceData, setPriceData] = useState<Object[]>([])
+    const [labels, setLabels] = useState<string[]>([])
+    const [priceData, setPriceData] = useState<string[]>([])
 
     const getAverages = (data: any) => {
+        console.log('data', data)
         const totalAverages: Object[] = []
+        const allDates: string[] = []
+        const allPrices: string[] = []
+
         for (const key in data) {
-            const average = {[key]: data[key]['4. close']}
+            // const average = {[key]: data[key]['4. close']}
+            const average = {name: key, closingPrice: data[key]['4. close']}
             totalAverages.push(average)
+            allDates.push(key)
+            allPrices.push(data[key]['4. close'])
         }
-        const halfLength = Math.ceil(totalAverages.length / 2); 
-        const leftSide = totalAverages.slice(0, halfLength); 
-        setPriceData(leftSide)
+        //const halfLength = Math.ceil(totalAverages.length / 2); 
+        //const leftSide = totalAverages.slice(0, halfLength); 
+        // setPriceData(leftSide.reverse())
+        setLabels(allDates)
+        setPriceData(allPrices)
     }
 
     const getStockInfo = (e: any) => {
@@ -44,6 +67,47 @@ export default function Home() {
         })
     }   
 
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Filler,
+        Legend
+      );
+      
+
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top' as const,
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+          },
+        },
+      };
+
+    // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+    const data = {
+      labels,
+      datasets: [
+            {
+            fill: true,
+            label: 'Dataset 2',
+            // data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            data: priceData,
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ]
+    };
+
     return (
         <>
             <h1>Search for a Stock</h1>
@@ -63,9 +127,9 @@ export default function Home() {
             <div>Stock Name: {stock}</div>
             <div>Earnings per Share: {eps}</div>
             <div>Quarterly Earnings Growth Year Over Year: {quarterlyEarningsGrowthYOY}</div>
-            <div className='graph'>
-
-            </div>
+            <GraphContainer>
+                <Line options={options} data={data} />
+            </GraphContainer>
         </>
 
     )
@@ -80,4 +144,14 @@ const StyledForm = styled.form`
 
 const StyledButton = styled(Button)`
     margin-left: 10px !important; 
+`
+
+const GraphContainer = styled.div`
+    height: 400px; 
+    width: 70%; 
+    margin: 0 auto; 
+
+    canvas {
+        margin: 0 auto; 
+    }
 `
