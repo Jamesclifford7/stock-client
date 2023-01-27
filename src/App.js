d30776a1e57bc82cc15fd13b1b114028d3a05ffb
James Clifford <jamesclifford7@gmail.com>
2023-01-27 16:51:56 -0700

    removed unnecessary 'withRouter' component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { Routes, Route, useNavigate, useLocation, useParams } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import Home from './components/Home/Home.tsx'
 import Login from './components/Login/Login.tsx'
 import UserProvider from './components/UserProvider';
@@ -43,35 +43,5 @@ function App() {
   );
 }
 
-const withRouter = (Component) => {
-  // const Wrapper = (props) => {
-  //   const history = useNavigate();
-    
-  //   return (
-  //     <Component
-  //       history={history}
-  //       {...props}
-  //       />
-  //   );
-  // };
-  
-  // return Wrapper;
-  
-  function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-    return (
-      <Component
-        {...props}
-        router={{ location, navigate, params }}
-      />
-    );
-  }
-
-  return ComponentWithRouterProp;
-}
-
-// export default withRouter(App);
 export default App;
 
